2a5dcb822efa94e809e77721ecd4d23144e0854b
JakZak <jakubzak93@wp.pl>
2018-10-07 08:57:44 +0200

    first commit

--- Python_bootcamp.md
+++ Python_bootcamp.md
@@ -1,1 +1,2 @@
 "# Python_bootcamp" 
+"# Python_bootcamp" 
--- zajzd _1/zadanie11.py
+++ zajzd _1/zadanie11.py
@@ -10,11 +10,38 @@ b=int(input("b: "))
 if a>x or b>y:
     print("O stary chyba troche wyleciałeś z planszy xD!!!")
 
-procent_10x = (10*x)/100
-procent_10y = (10*y)/100
+procent_10x = x*0.1
+print(procent_10x)
+procent_10y = y*0.1
+print(procent_10y)
 
-procent_90x = (90*x)/100
-procent_90y = (90*y)/100
+procent_90x = x*0.9
+print(procent_90x)
+procent_90y = y*0.9
+print(procent_90y)
+
+if a>procent_10x and a<procent_90x:
+    if b<procent_10y:
+        print("Znajdujesz sięw dolnej granicy planszy")
+    elif b>procent_90y:
+        print("Znajdujesz się w górnej granicy mapy")
+elif a<procent_10x:
+    if b>procent_10y and b<procent_90y:
+        print("Znajdujesz się w lewej części mapy")
+elif a>procent_90x:
+    if b>procent_10y and b<procent_90y:
+        print("Znajdujesz się w prawej części mapy")
+elif a<procent_10x and b<procent_10y:
+    print("Znajdujesz sięw lewym dolnym rogu")
+elif a > procent_90x and b < procent_10y:
+    print("Znajdujesz sięw prawym dolnym rogu")
+elif a<procent_10x and b>procent_90y:
+    print("Znajdujesz sięw lewym górnym rogu")
+elif a>procent_90x and b>procent_90y:
+    print("Znajdujesz się w prawym górnym rogu")
+else:
+    print("Jesteś w centrum mapy, UWAŻĄJ!")
+
 
 
 
