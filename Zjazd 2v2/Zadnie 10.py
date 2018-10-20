4457630e1e0d3ea294c4f0fab5037f90f301150c
JakZak <jakubzak93@wp.pl>
2018-10-20 12:11:50 +0200

    Zakończenie zadania łączenia słowników

--- Zjazd 2v2/Zadnie 10.py
+++ Zjazd 2v2/Zadnie 10.py
@@ -5,3 +5,22 @@ for z in napis:
 print(znaki)
 
 licznik_liter = {}
+
+for litera in napis:
+    if litera in licznik_liter:
+        licznik_liter[litera] += 1
+    elif litera not in licznik_liter:
+        if litera == " ":
+            pass
+        else:
+            licznik_liter[litera] = 1
+
+for z in licznik_liter:
+    print(f"W twoim napisie '{napis}', znak: [{z}] wystąpił \n{licznik_liter[z]} razy")
+
+
+#Wykonanie przez prowadzącego
+
+for litera in licznik_liter:
+    licznik_liter[litera] = licznik_liter.get(litera, 0) +1
+    print(f"W twoim napisie '{napis}', znak: [{litera}] wystąpił \n{licznik_liter[litera]} razy")
