76530be1e45e6c3359a38ef4dd646a6793b85bb9
JakZak <jakubzak93@wp.pl>
2018-11-03 14:39:53 +0100

    pracownik premium

--- Zjazd_3/Zadanie_3.py
+++ Zjazd_3/Zadanie_3.py
@@ -19,8 +19,24 @@ class Employee:
         self.time = 0
         return to_pay
 
+class PremiumEmployee(Employee):
 
+    def __init__(self, imie, nazwisko, stawka):
+        super().__init__(imie, nazwisko, stawka)
+        self.bonus = 0
 
+    def give_bonus(self, bonus):
+        self.bonus = bonus
+
+    def pay_salary(self):
+        to_pay = super().pay_salary()
+        return to_pay + self.bonus
+
+def test_premiumemployee_z_bonusem():
+    employee = PremiumEmployee('Jan', 'Nowak', 100.0)
+    employee.register_time(5)
+    employee.give_bonus(1000.0)
+    assert employee.pay_salary() == 1500.0
 
 def test_pracownik():
     employee = Employee('Jan', 'Nowak', 100.0)
